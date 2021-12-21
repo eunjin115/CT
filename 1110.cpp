22e784ae55513490d005a12c8c53a320e79ee4ca
eunjin115 <katherine0115@naver.com>
2021-12-21 15:20:39 +0900

    update

--- 1110.cpp
+++ 1110.cpp
@@ -4,5 +4,24 @@ using namespace std;
 
 int main()
 {
-	
+	int n, a, b, ans = 0, tmp = 0, cnt = 0;
+
+	cin >> n;
+	tmp = n;
+
+	while (!(ans == tmp))
+	{
+		a = n / 10;
+		b = n % 10; 
+		ans = a + b;
+		b = b * 10;
+		a = ans % 10;
+		ans = a + b;
+		n = ans;
+		cnt++;
+	}
+
+	cout << cnt;
+
+	return (0);
 }
