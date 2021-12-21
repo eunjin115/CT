961f31245a205db42107958497a473c4e89bf789
eunjin115 <katherine0115@naver.com>
2021-12-21 13:43:43 +0900

    update

---
+++ 10871.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, x;
+	cin >> n >> x;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < n; i++)
+	{
+		if (x > arr[i])
+			cout << arr[i] << " ";
+	}
+	
+	return (0);
+}
