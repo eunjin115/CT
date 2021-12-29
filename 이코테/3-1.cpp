f652dd83a06f22ff4ada45ae8b70c9a1376e1096
eunjin115 <katherine0115@naver.com>
2021-12-29 12:00:21 +0900

    update files

---
+++ 이코테/3-1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main()
+{
+	vector<int> coin_type = {500,100,50,10};
+	int n = 1260, cnt = 0, tmp; 
+	for(int i = 0; i < coin_type.size(); i++)
+	{
+		tmp = coin_type[i];
+		cnt += n / tmp;
+		n %= tmp;
+	}
+
+	cout << cnt;
+}
