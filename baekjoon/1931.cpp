4077996297ed4daff5acfa38f1f8085e2467b2e7
eunjin115 <katherine0115@naver.com>
2021-12-31 11:29:31 +0900

    update greedy

--- baekjoon/1931.cpp
+++ baekjoon/1931.cpp
@@ -6,10 +6,27 @@ using namespace std;
 
 int main()
 {
-	int n;
+	int n, time, result = 1;
 	cin >> n;
 
-	vector<pair<int, int>> a(n);
+	vector<pair<int, int>> meet_time(n);
 
+	for (int i = 0; i < n; i++)
+	{
+		//cin >> meet_time[i].first >> meet_time[i].second; //시작시간 : first, 종료시간 : second
+		cin >> meet_time[i].second >> meet_time[i].first; //시작시간 : first, 종료시간 : second
 
+	}
+	sort(meet_time.begin(), meet_time.end());
+
+	time = meet_time[0].first;
+	for (int i = 1; i < n; i++)
+	{
+		if (meet_time[i].second >= time)
+		{
+			result++;
+			time = meet_time[i].first;
+		}
+	}
+	cout << result;
 }
