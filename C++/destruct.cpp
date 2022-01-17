a2b84260e0403c98225fd6bc3e98c2c4f84e86ce
Eunjin <katherine0115@naver.com>
2022-01-17 18:43:47 +0900

    Upload files

---
+++ C++/destruct.cpp
@@ -0,0 +1,28 @@
+//#include <iostream>
+//
+//class test
+//{
+//private:
+//	int b;
+//	char c;
+//public:
+//	int a;
+//
+//	test() : a(1), b(2), c('a'){}
+//
+//	~test() { std::cout << "deleting now \n\n"; }
+//};
+//
+//int main()
+//{
+//	int* a;
+//	{
+//		test t;
+//		getchar();
+//		a = &t.a;
+//	}
+//	std::cout << a << "\n";
+//	std::cout << *a << "\n";
+//
+//	return 0;
+//}
