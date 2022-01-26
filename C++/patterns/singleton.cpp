2a021597c37093688695ef4f7b6a15e898f5164c
eunjin115 <katherine0115@naver.com>
2022-01-26 18:26:14 +0900

    update patterns

--- C++/patterns/singleton.cpp
+++ C++/patterns/singleton.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include <memory>
+
+class Singleton
+{
+private:
+	Singleton()
+	{
+
+	}
+	~Singleton()
+	{
+		
+	}
+public:
+	static std::shared_ptr<Singleton> pInst;
+};
+
+
--- C++/patterns/test.cpp
+++ C++/patterns/test.cpp
@@ -1,20 +1,27 @@
 #include <iostream>
 
-class A
-{
+class Singleton {
+private:
+    static Singleton s;
 public:
-	virtual void Function() = 0;
-	virtual void Function1();
-	void Function2();
+    static Singleton& getIncetance() 
+	{
+		std::cout <<"what? \n";
+        return s;
+    }
 };
 
-class B : public A
-{
-
+class A {
+public:
+    A() {
+        Singleton& s = Singleton::getIncetance();
+    }
 };
 
-int main()
-{
-	B b;
+Singleton Singleton::s; //전역변수로써 초기화
+
+int main(void) {
+    //Singleton& s = Singleton::getIncetance();
+    A a;
 	return 0;
 }
