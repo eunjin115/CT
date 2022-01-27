901e3042fb97b915f55a28ffd64dcf605c66b39a
eunjin115 <katherine0115@naver.com>
2022-01-27 11:02:01 +0900

    update singleton

--- C++/patterns/singleton.cpp
+++ C++/patterns/singleton.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 #include <memory>
+#include <string>
 
-class Singleton
+class SingletonPrint
 {
 private:
-	Singleton()
+	static std::shared_ptr<SingletonPrint> pInst; //객체를 참조 할 정적 참조변수 
+	SingletonPrint() //생성자를 외부에서 사용하지 못하도록 함 
 	{
 
 	}
-	~Singleton()
+	~SingletonPrint()
 	{
 		
 	}
 public:
-	static std::shared_ptr<Singleton> pInst;
+	static std::shared_ptr<SingletonPrint> GetInstance()
+	{
+		if (pInst == nullptr)
+		{
+			pInst = std::make_shared<SingletonPrint>();
+		}
+		return pInst;
+	}
+
+	void printing(std::string str)
+	{
+		std::cout << "Printing now : " << str << "\n";
+	}
+
 };
 
+int main()
+{
+	std::shared_ptr<SingletonPrint> print = SingletonPrint::GetInstance();
+	print->printing("You know even when i said moved on \n");
 
+	return 0;
+}
