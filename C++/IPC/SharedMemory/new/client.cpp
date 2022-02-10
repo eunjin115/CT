826bc9853d09600194acaf8044b765973de9b263
eunjin115 <katherine0115@naver.com>
2022-02-10 18:35:08 +0900

    update files - 코드 정리

--- C++/IPC/SharedMemory/new/client.cpp
+++ C++/IPC/SharedMemory/new/client.cpp
@@ -2,11 +2,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
 #include <memory>
 #include <thread>
 
+LPTSTR Mutex = TEXT("Mutex"); //named mutex
+LPTSTR shared_memory = TEXT("SharedMemory");
+
 HANDLE hMutex;
+HANDLE hMap;
+
 struct TestData
 {
     int file_size;
@@ -15,14 +19,13 @@ struct TestData
     char buffer[1200000054] = { 0 }; //최대 1GB
 };
 
-
-void SendFile(TestData*& shared_data);
-void RecvFile(TestData*& shared_data);
-
-int main()
+TestData* initial()
 {
-    //Named SharedMemory 정의 (두 프로세스 모두 있어야 한다) 
-    LPTSTR shared_memory = TEXT("SharedMemory1"); //server write to client 
+    hMutex = OpenMutex(
+        MUTEX_ALL_ACCESS,
+        FALSE,
+        Mutex
+    );
 
     //Client는 SharedMemory를 open하여 사용 
     HANDLE hMap = OpenFileMapping( //first shared memory 
@@ -30,80 +33,30 @@ int main()
         FALSE,
         shared_memory);
 
-
     if (hMap == NULL) //error 처리 
     {
         std::cout << "Could not find file mapping object (" << GetLastError() << ").\n";
-        return 1;
+        return NULL;
     }
 
     //SharedMemory에 대한 실제 메모리 매핑 
-    void* mapped_memory = MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    TestData* shared_data = (TestData*)MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
 
-    if (mapped_memory == NULL ) //error 처리 
+    if (shared_data == NULL) //error 처리 
     {
         std::cout << "Could not map view of file (" << GetLastError() << ").\n";
         CloseHandle(hMap);
-
-        return 1;
+        return NULL;
     }
 
-    TestData* shared_data = (TestData*)mapped_memory;
-
-    char input;
-    std::string path;
-
-    LPTSTR Mutex = TEXT("Mutex"); //named mutex
-
-    hMutex = OpenMutex(
-        MUTEX_ALL_ACCESS,
-        FALSE,
-        Mutex
-    );
-
-    while (1)
-    {
-        //S or R 입력받기        
-        std::cout << "Please Enter Command. \n S - Send file, R - Recive file, Q - Exit \n";
-        std::cin >> input;
-
-        if (input == 'S' || input == 's')
-        {
-            std::cout << "Enter File Name : ";
-            std::cin >> shared_data->path;
-
-            std::thread file_send = std::thread(SendFile, std::ref(shared_data));
-
-            //SendFile(shared_data);
-            file_send.detach();
-
-            continue;
-        }
-        else if (input == 'R' || input == 'r')
-        {
-            std::thread file_recv = std::thread(RecvFile, std::ref(shared_data));
-
-            //RecvFile(shared_data);
-            file_recv.detach();
-            continue;
-        }
-        else if (input == 'Q' || input == 'q')
-        {
-            break;
-        }
-        else
-        {
-            std::cout << "Check command again. \n";
-            continue;
-        }
-    }
-
-    return 0;
+    return shared_data;
 }
+
 
 void SendFile(TestData*& shared_data) //동기화 객체 추가하기 
 {
     WaitForSingleObject(hMutex, INFINITE);
+    ::Sleep(10000); //test
 
     std::ifstream fin;
 
@@ -121,11 +74,13 @@ void SendFile(TestData*& shared_data) //동기화 객체 추가하기
 
 }
 
-
 void RecvFile(TestData*& shared_data)
 {
     std::ofstream fout;
     WaitForSingleObject(hMutex, INFINITE);
+
+    ::Sleep(10000); //test
+
 
     std::string path;
     std::cout << "Enter New File Name : ";
@@ -143,4 +98,56 @@ void RecvFile(TestData*& shared_data)
     std::cout << "Read Done \n\n";
     ReleaseMutex(hMutex);
 
+}
+
+
+int main()
+{
+    TestData* shared_data = initial();
+
+    if (shared_data == NULL) //error
+        return 1;
+
+    char input;
+    std::string path;
+
+
+    while (1)
+    {
+        //S or R 입력받기        
+        std::cout << "Please Enter Command. \n S - Send file, R - Recive file, Q - Exit \n";
+        std::cin >> input;
+
+        if (input == 'S' || input == 's')
+        {
+            std::cout << "Enter File Name : ";
+            std::cin >> shared_data->path;
+
+            std::thread file_send = std::thread(SendFile, std::ref(shared_data));
+            file_send.detach();
+
+            continue;
+        }
+        else if (input == 'R' || input == 'r')
+        {
+            std::thread file_recv = std::thread(RecvFile, std::ref(shared_data));
+            file_recv.detach();
+            continue;
+        }
+        else if (input == 'Q' || input == 'q')
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Check command again. \n";
+            continue;
+        }
+    }
+
+    ::UnmapViewOfFile(shared_data);
+    ::CloseHandle(hMap);
+    ::CloseHandle(hMutex);
+
+    return 0;
 }
--- C++/IPC/SharedMemory/new/server.cpp
+++ C++/IPC/SharedMemory/new/server.cpp
@@ -3,10 +3,13 @@
 #include <fstream>
 #include <string>
 #include <memory>
-
 #include <thread>
 
+LPTSTR Mutex = TEXT("Mutex"); //named mutex
+LPTSTR shared_memory = TEXT("SharedMemory"); 
+
 HANDLE hMutex;
+HANDLE hMap;
 
 struct TestData
 {
@@ -19,90 +22,13 @@ struct TestData
 void SendFile(TestData*& shared_data);
 void RecvFile(TestData*& shared_data);
 
-int main()
-{
-    //FMO = File Mapping Object
-    //Named SharedMemory 정의 (두 프로세스 모두 있어야 한다) 
-    LPTSTR shared_memory = TEXT("SharedMemory1"); //server write to client 
-
-    //Server는 첫 번째 SharedMemory만을 생성
-    HANDLE hMap = CreateFileMapping( //first shared memory 
-        INVALID_HANDLE_VALUE,
-        NULL,
-        PAGE_READWRITE,
-        0,
-        sizeof(TestData),
-        shared_memory);
-
-
-    if (hMap == NULL) //error 처리 
-    {
-        std::cout << "Could not find file mapping object (" << GetLastError() << ").\n";
-        return 1;
-    }
-
-    //SharedMemory에 대한 실제 메모리 매핑 
-    void* mapped_memory = MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
-
-    if (mapped_memory == NULL)
-    {
-        std::cout << "Could not map view of file (" << GetLastError() << ").\n";
-        CloseHandle(hMap);
-        return 1;
-    }
-
-    TestData* shared_data = (TestData*)mapped_memory;
-
-    LPTSTR Mutex = TEXT("Mutex"); //named mutex
-    hMutex = CreateMutex(
-        NULL,
-        FALSE,
-        Mutex);
-
-    char input;
-    std::string path;
-    while (1)
-    {
-        //S or R 입력받기        
-        std::cout << "Please Enter Command. \n S - Send file, R - Recive file, Q - Exit \n";
-        std::cin >> input;
-
-        if (input == 'S' || input == 's')
-        {
-            std::cout << "Enter File Name : ";
-            std::cin >> shared_data->path;
-
-            std::thread file_send = std::thread(SendFile, std::ref(shared_data));
-
-            file_send.detach();
-            //SendFile(shared_data);
-            continue;
-        }
-        else if (input == 'R' || input == 'r')
-        {
-            RecvFile(shared_data);
-            continue;
-        }
-        else if (input == 'Q' || input == 'q')
-        {
-            break;
-        }
-        else
-        {
-            std::cout << "Check command again. \n";
-            continue;
-        }
-    }
-
-    return 0;
-}
 
 void SendFile(TestData*& shared_data) //동기화 객체 추가하기 
 {
     std::ifstream fin;
 
     WaitForSingleObject(hMutex, INFINITE);
-    //::Sleep(10000);
+    ::Sleep(10000); //test
     fin.open(shared_data -> path, std::ios::binary);
     fin.seekg(0, std::ios::end);
 
@@ -120,6 +46,9 @@ void SendFile(TestData*& shared_data) //동기화 객체 추가하기
 void RecvFile(TestData*& shared_data) 
 {
     std::ofstream fout;
+    WaitForSingleObject(hMutex, INFINITE);
+
+    ::Sleep(10000); //test
 
     std::string path;
     std::cout << "Enter New File Name : ";
@@ -135,4 +64,92 @@ void RecvFile(TestData*& shared_data)
     }
 
     std::cout << "Read Done \n\n";
+    ReleaseMutex(hMutex);
+}
+
+TestData* initial()
+{
+    hMutex = CreateMutex(
+        NULL,
+        FALSE,
+        Mutex);
+
+    //Server는 첫 번째 SharedMemory만을 생성
+    HANDLE hMap = CreateFileMapping( //first shared memory 
+        INVALID_HANDLE_VALUE,
+        NULL,
+        PAGE_READWRITE,
+        0,
+        sizeof(TestData),
+        shared_memory);
+
+    if (hMap == NULL) //error 처리 
+    {
+        std::cout << "Could not find file mapping object (" << GetLastError() << ").\n";
+        return NULL;
+    }
+
+    //SharedMemory에 대한 실제 메모리 매핑 
+    void* mapped_memory = MapViewOfFile(hMap, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
+    if (mapped_memory == NULL)
+    {
+        std::cout << "Could not map view of file (" << GetLastError() << ").\n";
+        CloseHandle(hMap);
+        return NULL;
+    }
+
+    TestData* shared_data = (TestData*)mapped_memory;
+
+    return shared_data;
+}
+
+
+int main()
+{
+    TestData* shared_data = initial();
+
+    if (shared_data == NULL) //error
+        return 1;
+
+    char input;
+    std::string path;
+    while (1)
+    {
+        //S or R 입력받기        
+        std::cout << "Please Enter Command. \n S - Send file, R - Recive file, Q - Exit \n";
+        std::cin >> input;
+
+        if (input == 'S' || input == 's')
+        {
+            std::cout << "Enter File Name : ";
+            std::cin >> shared_data->path;
+
+            std::thread file_send = std::thread(SendFile, std::ref(shared_data));
+            ::Sleep(1000);
+            file_send.detach();
+            continue;
+        }
+        else if (input == 'R' || input == 'r')
+        {
+            std::thread file_recv = std::thread(RecvFile, std::ref(shared_data));
+            file_recv.detach();
+            continue;
+        }
+        else if (input == 'Q' || input == 'q')
+        {
+            break;
+        }
+        else
+        {
+            std::cout << "Check command again. \n";
+            continue;
+        }
+    }
+
+    ::UnmapViewOfFile(shared_data);
+    ::CloseHandle(hMap);
+    ::CloseHandle(hMutex);
+
+    return 0;
 }
